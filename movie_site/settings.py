71eff0d20b82f8bc057454392196415dd3781e57
10nayan <nayanjyotipatowary@gmail.com>
2020-08-09 12:56:39 +0530

    reset

--- movie_site/settings.py
+++ movie_site/settings.py
@@ -78,8 +78,8 @@ WSGI_APPLICATION = 'movie_site.wsgi.application'
 
 DATABASES = {}
 import dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default']=db_from_env
 
 
 # Password validation
