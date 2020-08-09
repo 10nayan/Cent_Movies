90bcc17d2970c6d8ceb467731a738545175b3c34
10nayan <nayanjyotipatowary@gmail.com>
2020-08-09 12:11:09 +0530

    secret_key added again

--- movie_site/settings.py
+++ movie_site/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'movie_site.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES[‘default’].update(db_from_env)
+DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
