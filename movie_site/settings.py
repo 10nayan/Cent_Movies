a031167198f13f94d7478ee33448edf5b4867a40
10nayan <nayanjyotipatowary@gmail.com>
2020-10-16 14:17:23 +0530

    new

--- movie_site/settings.py
+++ movie_site/settings.py
@@ -82,7 +82,7 @@ import dj_database_url
 try:
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default']=db_from_env
-else:
+except:
     DATABASES['default']={
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
