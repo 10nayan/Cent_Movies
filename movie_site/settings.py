b6ae18b6ccbafb93b5bb0a166f5162597fb64415
10nayan <nayanjyotipatowary@gmail.com>
2020-10-16 14:24:37 +0530

    bug fixed

--- movie_site/settings.py
+++ movie_site/settings.py
@@ -74,20 +74,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'movie_site.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+"""
+WARNING
+(use following database setting for development server)
 
 DATABASES = {}
 import dj_database_url
-try:
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default']=db_from_env
-except:
-    DATABASES['default']={
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default']=db_from_env
 """
 DATABASES = {
     'default': {
