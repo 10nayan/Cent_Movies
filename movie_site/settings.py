ec53d56e9a640331e6605b0e9551866f3a852ab3
10nayan <nayanjyotipatowary@gmail.com>
2020-10-16 14:10:47 +0530

    new change

--- movie_site/settings.py
+++ movie_site/settings.py
@@ -79,8 +79,14 @@ WSGI_APPLICATION = 'movie_site.wsgi.application'
 
 DATABASES = {}
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default']=db_from_env
+try:
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES['default']=db_from_env
+else:
+    DATABASES['default']={
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 
 """
 DATABASES = {
