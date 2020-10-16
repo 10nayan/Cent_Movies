01d4e40b306b82b3deb43a18094eea236694d22c
Nayan Jyoti Patowary <nayanjyotipatowary@gmail.com>
2020-10-16 12:13:00 +0530

    must be changed

--- movie_site/settings.py
+++ movie_site/settings.py
@@ -76,11 +76,18 @@ WSGI_APPLICATION = 'movie_site.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+"""
 DATABASES = {}
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default']=db_from_env
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
