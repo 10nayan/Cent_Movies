6196cd1b1ab44cea65c5564603ee370d372c6f39
10nayan <nayanjyotipatowary@gmail.com>
2020-10-16 13:43:26 +0530

    settings file fixed

--- movie_site/settings.py
+++ movie_site/settings.py
@@ -76,11 +76,12 @@ WSGI_APPLICATION = 'movie_site.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-"""
+
 DATABASES = {}
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default']=db_from_env
+
 """
 DATABASES = {
     'default': {
@@ -89,7 +90,7 @@ DATABASES = {
     }
 }
 
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
